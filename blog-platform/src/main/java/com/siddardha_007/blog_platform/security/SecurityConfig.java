1171d503ce846c6394320a525396eb78678c11e0
siddardha-007 <siddardhabvs@gmail.com>
2026-06-01 00:15:35 +0530

    4th commit after adding swagger

--- blog-platform/src/main/java/com/siddardha_007/blog_platform/security/SecurityConfig.java
+++ blog-platform/src/main/java/com/siddardha_007/blog_platform/security/SecurityConfig.java
@@ -26,7 +26,10 @@ public class SecurityConfig {
         http
                 .csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(auth->auth
-                        .requestMatchers("/api/auth/**")
+                        .requestMatchers("/api/auth/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/v3/api-docs/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
